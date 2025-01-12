40cb14b2d1ce028607c38b6a525438eec3c662d7
Gabriel Dalton <gmanpixelpro@gmail.com>
2025-01-11 21:46:56 -0800

    updated animation of settings gear

--- Daily_Updates/assets/settings.js
+++ Daily_Updates/assets/settings.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const gearButton = document.getElementById("gearButton");
-  
-    gearButton.addEventListener("click", () => {
+  const gearButton = document.getElementById("gearButton");
+  const clearDataModal = document.getElementById("clearDataModal");
+
+  let hasHovered = false;
+
+  gearButton.addEventListener("mouseover", () => {
+    if (!hasHovered) {
       gearButton.classList.add("spin-on-click");
-  
+      hasHovered = true;
+
       gearButton.addEventListener(
         "animationend",
         () => {
@@ -11,6 +16,19 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         { once: true }
       );
-    });
+    }
   });
-  
+
+  gearButton.addEventListener("click", () => {
+    gearButton.classList.add("spin-on-click");
+
+    gearButton.addEventListener(
+      "animationend",
+      () => {
+        gearButton.classList.remove("spin-on-click");
+        clearDataModal.classList.remove("hidden"); 
+      },
+      { once: true }
+    );
+  });
+});
